3d143785ad07f723cd5a4e8b42ae4e7310bd47da
RexterR <sumandas.workplace@gmail.com>
2020-11-19 15:40:15 +0530

    export all Error classes

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-import "./errors/BadRequestError"
+import { BadRequestError } from "./errors/BadRequestError";
+import { AuthorizationError } from "./errors/AuthorizationError";
+import { CustomError } from "./errors/CustomError";
+export { BadRequestError, AuthorizationError, CustomError };
